b5ef3b19750479ab7d5fe95e3692fa96812419c9
Technicus <fgardner@madisoncollege.edu>
2015-08-14 16:31:29 -0500

    initial commit

---
+++ software/code/tiny_sleep_test/tiny_sleep_test/tiny_sleep_test.c
@@ -0,0 +1,16 @@
+/*
+ * tiny_sleep_test.c
+ *
+ * Created: 7/10/2015 9:44:14 AM
+ *  Author: benheck
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
